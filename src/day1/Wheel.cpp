923cc01a87f8344c5df8c6e89c0cc7b4414ccde7
BiomechatronicsRookie <victor.carmona.ortiz@gmail.com>
2025-12-08 12:26:36 +0100

    work in progress day1 pt 2

--- src/day1/Wheel.cpp
+++ src/day1/Wheel.cpp
@@ -12,13 +12,14 @@ int Wheel::rotate(int dial, int* pwd){
     // Declare variables to store password steps and errors
     int temp =0;
     int move = 0;
+    int extra_0 = 0;
     std::string c;
     std::string num = "0000";
     std::ios_base::iostate err = 0;
     std::vector<int> range {0,99};
 
     // Open the input file
-    this->inFile.open("C:\\Users\\Carsten\\git\\vic\\advent-of-code\\src\\day1\\input.txt");
+    this->inFile.open("C:\\Users\\Carsten\\git\\vic\\advent-of-code\\src\\day1\\test.txt");
 
     // Read the file until rdstate returns end of file bit (returns 1)
     while(!this->inFile.rdstate()){
@@ -28,11 +29,18 @@ int Wheel::rotate(int dial, int* pwd){
         // If the command is L, rotation is negative, else is positive
         move = c[0] == 'L' ? ~temp+1 : temp;
         dial += move;
-        while((dial > 99) || (dial < 0)){
-            wrap(&dial, range);
+
+        if (!dial){
+            *pwd += 1;
+        }else{
+            extra_0 = 0;
+            while(!((dial <= 99) && (dial >= 0))){
+                wrap(&dial, range);
+                extra_0 += 1;
+                *pwd += 1;
+            }
         }
-        *pwd = !dial ? *pwd+1 : *pwd;
-        std::cout << "Move is: " << move << ' ' << "Curr Pos is: " << dial << ' ' << "Curr Pwd is: " << *pwd << std::endl;
+        std::cout << "Move is: " << move << ' ' << "Curr Pos is: " << dial << ' ' << "Curr Pwd is: " << *pwd << ' ' << "Passed through 0: " << extra_0 <<  std::endl;
     }
     // If end of file reached return 0 else return 1
     if (this->inFile.rdstate()){
