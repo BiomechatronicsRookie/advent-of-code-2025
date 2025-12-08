452efed2c449d7e9bec5c3b80a9e71383feec176
BiomechatronicsRookie <victor.carmona.ortiz@gmail.com>
2025-12-08 17:04:42 +0100

    Works for test but not for input

--- src/day1/Wheel.cpp
+++ src/day1/Wheel.cpp
@@ -12,7 +12,6 @@ int Wheel::rotate(int dial, int* pwd){
     // Declare variables to store password steps and errors
     int temp =0;
     int move = 0;
-    int extra_0 = 0;
     std::string c;
     std::string num = "0000";
     std::ios_base::iostate err = 0;
@@ -20,6 +19,7 @@ int Wheel::rotate(int dial, int* pwd){
 
     // Open the input file
     this->inFile.open("C:\\Users\\Carsten\\git\\vic\\advent-of-code\\src\\day1\\test.txt");
+    std::cout << move << ' ' << dial << ' ' << *pwd <<  std::endl;
 
     // Read the file until rdstate returns end of file bit (returns 1)
     while(!this->inFile.rdstate()){
@@ -27,20 +27,19 @@ int Wheel::rotate(int dial, int* pwd){
         num = c.substr(1,3);
         temp = std::stoi(num);
         // If the command is L, rotation is negative, else is positive
-        move = c[0] == 'L' ? ~temp+1 : temp;
+        move = (c[0] == 'L') ? ~temp+1 : temp;
+        *pwd = ((c[0] == 'L') && (dial == 0)) ? *pwd-1 : *pwd;
         dial += move;
 
         if (!dial){
             *pwd += 1;
         }else{
-            extra_0 = 0;
-            while(!((dial <= 99) && (dial >= 0))){
+            while((dial > 99) || (dial < 0)){
                 wrap(&dial, range);
-                extra_0 += 1;
-                *pwd += 1;
+                *pwd  += 1;
             }
         }
-        std::cout << "Move is: " << move << ' ' << "Curr Pos is: " << dial << ' ' << "Curr Pwd is: " << *pwd << ' ' << "Passed through 0: " << extra_0 <<  std::endl;
+        std::cout << c << ' ' << dial  << ' ' << *pwd <<  std::endl;
     }
     // If end of file reached return 0 else return 1
     if (this->inFile.rdstate()){
@@ -52,7 +51,7 @@ int Wheel::rotate(int dial, int* pwd){
 
 void wrap(int* pos, std::vector<int> range){
     if (*pos < range[0]){
-        *pos = 100 + *pos;
+        *pos += 100;
     } else if(*pos > range[1]){
         *pos -= 100;
     };
